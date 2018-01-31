0ce5430970e84fa0f05da3bdb2e518eb1d77c3cf
Ray Monterola <renz.monterola@gmail.com>
2018-01-31 10:20:04 +0800

    improvements

--- module3-coding-assignment/scripts/app.js
+++ module3-coding-assignment/scripts/app.js
@@ -9,52 +9,57 @@
   NarrowItDownController.$inject = ["MySearchService"];
   function NarrowItDownController (MySearchService) {
       var nidc = this;
-      nidc.found = [];
       nidc.searchTerm = "";
       nidc.narrowItDown = function () {
           var promise = MySearchService.getMatchedMenuItems(nidc.searchTerm);
           promise.then(function (response) {
               nidc.found = response;
-              showOrHideMessage();
+          }, function (response) {
+              nidc.found = [];
           });
       };
       nidc.removeItem = function (index) {
           nidc.found.splice(index, 1);
-          showOrHideMessage();
       };
       nidc.showNotFound = false;
-      function showOrHideMessage () {
-          nidc.showNotFound = nidc.found.length < 1;
-      }
   }
 
-  MySearchService.$inject = ["$http"];
-  function MySearchService ($http) {
+  MySearchService.$inject = ["$http", "$q"];
+  function MySearchService ($http, $q) {
       var mss = this;
 
       mss.getMatchedMenuItems = function (searchTerm) {
-          return $http({
-              url: "https://davids-restaurant.herokuapp.com/menu_items.json",
-              method: "GET"
-          }).then(
-              function (response) {
-                  var menuItems = [];
-                  if (response.data.hasOwnProperty("menu_items") && searchTerm) {
-                      menuItems = response.data.menu_items;
-                      searchTerm = searchTerm.toLowerCase();
-                      var filteredItems = [];
-                      for (var i = 0; i < menuItems.length; i++) {
-                          if (menuItems[i].hasOwnProperty("description") &&
-                              menuItems[i].description.indexOf(searchTerm) > -1) {
-                              filteredItems.push(menuItems[i]);
+          var deferred = $q.defer();
+          if (searchTerm) {
+              $http({
+                  url: "https://davids-restaurant.herokuapp.com/menu_items.json",
+                  method: "GET"
+              }).then(
+                  function (response) {
+                      var menuItems = [];
+                      if (response.data.hasOwnProperty("menu_items")) {
+                          menuItems = response.data.menu_items;
+                          searchTerm = searchTerm.toLowerCase();
+                          var filteredItems = [];
+                          for (var i = 0; i < menuItems.length; i++) {
+                              if (menuItems[i].hasOwnProperty("description") &&
+                                  menuItems[i].description.indexOf(searchTerm) > -1) {
+                                  filteredItems.push(menuItems[i]);
+                              }
                           }
+                          deferred.resolve(filteredItems);
+                      } else {
+                          deferred.resolve(menuItems);
                       }
-                      return filteredItems;
-                  } else {
-                      return menuItems;
+                  },
+                  function (response) {
+                      deferred.reject([]);
                   }
-              }
-          );
+              );
+          } else {
+              deferred.reject([]);
+          }
+          return deferred.promise;
       };
   }
 
@@ -63,9 +68,28 @@
           templateUrl: "templates/found-items.html",
           scope: {
               found: "<",
-              removeItem: "&"
+              removeItem: "&",
           },
-          restrict: "E"
+          restrict: "E",
+          link: FoundItemsLink
       }
+  }
+
+  function FoundItemsLink(scope, element, attrs, controller) {
+      scope.$watch(
+          function () {
+              if (scope.found) {
+                  return scope.found.length < 1;
+              }
+          },
+          function (newValue, oldValue) {
+              if (newValue === true) {
+                  element.find("h5#error").slideDown(1000);
+              } else {
+                  element.find("h5#error").slideUp(50);
+                  element.find(".menu-item").slideDown(1000);
+              }
+          }
+      );
   }
 })();
